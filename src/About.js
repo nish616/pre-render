b202809fa14d8e5b10533d973afdcb496e6034a1
nishin616 <nishin.s@analyticsindiamag.com>
2021-07-05 15:08:08 +0530

    Init

---
+++ src/About.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <img src="https://wallpaperaccess.com/full/170249.jpg"></img>
+        </div>
+    
+    );
+  }
